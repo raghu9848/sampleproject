b17ee8ffe45017d006ce50b5c6a792ba11269594
raghu9848 <raghub785@gmail.com>
2015-11-22 21:25:56 -0500

    total project commited
    total project

---
+++ date.js
@@ -0,0 +1,7 @@
+var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+
+    var yyyy = today.getFullYear();
+    if(dd<10){dd='0'+dd} if(mm<10){mm='0'+mm} var today = mm.toString()+" "+dd+","+yyyy;
+    document.getElementById("date").innerHTML = today;
